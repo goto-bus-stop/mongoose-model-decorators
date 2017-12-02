a9e11ee8dba50988b3f9f6b9e4bbf280bc2b11b6
Renée Kooi <renee@kooi.me>
2017-12-02 16:31:54 +0100

    fix babel config

--- .babelrc.js
+++ .babelrc.js
@@ -4,6 +4,7 @@ module.exports = {
   ],
   plugins: [
     '@babel/plugin-proposal-decorators',
+    '@babel/plugin-proposal-class-properties',
     '@babel/plugin-proposal-object-rest-spread'
   ]
 }
